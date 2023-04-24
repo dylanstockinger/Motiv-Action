01afe1cd8b4285eaee7cafaa1d05965537808c0b
Dylan Stockinger <pro.stockinger@gmail.com>
2023-04-24 22:09:27 +0200

    ✨ 🎨

--- motiv-action/src/components/Navbar/NavBar.tsx
+++ motiv-action/src/components/Navbar/NavBar.tsx
@@ -1,8 +1,14 @@
 import * as React from "react";
 import "./Navbar.css";
+import MyIcon from "../../assets/images/Group 1.svg";
 const NavBar: React.FC = () => {
   return (
     <nav className="topnav">
+      <div>
+        {" "}
+        <MyIcon />{" "}
+      </div>
+
       <a className="active" href="#home">
         Home
       </a>
