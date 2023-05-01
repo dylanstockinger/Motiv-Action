671e37082053ea967d93e41948c19d50b6fef1da
Brice Vignal <vignal.brice@gmail.com>
2023-05-01 15:30:15 +0200

    feat(router - app)

--- motiv-action/src/App.tsx
+++ motiv-action/src/App.tsx
@@ -1,33 +1,10 @@
-import { useEffect } from "react";
-import Card from "@/components/Card/Card";
-import NavBar from "@/components/Navbar/NavBar";
-import { getRandomFamousQuotes } from "./api/famousQuotes";
-import { Actions, useFamousQuote } from "./store/famousQuotes";
-
+import AppRoutes from "./router";
+import Layout from "./layout/Layout";
 function App() {
-  const [famousQuote, dispatchFamousQuote] = useFamousQuote();
-
-  useEffect(() => {
-    let ignore = false;
-    async function getRandomQuote() {
-      const { randomFamousQuotes } = await getRandomFamousQuotes();
-      if (ignore === false)
-        dispatchFamousQuote({
-          payload: randomFamousQuotes,
-          type: Actions.SET_FAMOUS_QUOTE,
-        });
-    }
-    getRandomQuote();
-    return () => {
-      ignore = true;
-    };
-  }, [dispatchFamousQuote]);
-
   return (
-    <div className="app ">
-      <NavBar />
-      {famousQuote && <Card famousQuote={famousQuote} />}
-    </div>
+    <Layout>
+      <AppRoutes />
+    </Layout>
   );
 }
 
