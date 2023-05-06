129018dcf3b544aac9cc361bd09113a530a73557
Brice Vignal <vignal.brice@gmail.com>
2023-05-06 15:41:18 +0200

    fix(tailwindcss - pckgs): fix dependencies

--- motiv-action/postcss.config.js
+++ motiv-action/postcss.config.js
@@ -1,8 +1,7 @@
-export default { plugins : {
-    tailwindcss : {},
-    autoprefixer:{},
-    "postcss-import":{},
-    "tailwindcss/nesting":{},
-    }
-}
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import tailwindConfig from "./tailwind.config.js";
 
+export default {
+  plugins: [autoprefixer, tailwind(tailwindConfig)],
+};
